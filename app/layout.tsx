6c7eb736aff9bcea218f3df198d9b20fc13953da
zguiyang <zhaoguiyang18@outlook.com>
2026-04-11 13:16:50 +0800

    refactor(app): flatten (auth) route group structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,8 @@ export default function RootLayout({
       <body
         className="font-inter font-manrope min-h-full flex flex-col antialiased"
       >
+        {/* TODO: Global ThemeProvider */}
+        {/* TODO: Global Context Providers (e.g., User, Theme, etc.) */}
         {children}
       </body>
     </html>
